efe5ea141bb5c25b6e4ea83f24e0615657749b05
avaverin <averin.av@edu.spbstu.ru>
2023-02-20 17:00:57 +0300

    fix: -h key

--- 01_vcs/src/cli.cpp
+++ 01_vcs/src/cli.cpp
@@ -5,47 +5,58 @@
 #include "vcs.h"
 
 bool handle_init_command(const std::vector<std::string>& args) {
-  if (!args.empty()) {
-    std::cerr << "Error: init command does not take any arguments." << std::endl;
-    return false;
-  }
+    if (!args.empty()) {
+        std::cerr << "Error: init command does not take any arguments." << std::endl;
+        return false;
+    }
 
-  if (!vcs_init()) {
-    std::cerr << "Error: repository already exists." << std::endl;
-    return false;
-  }
+    if (!vcs_init()) {
+        std::cerr << "Error: repository already exists." << std::endl;
+        return false;
+    }
 
-  std::cout << "Repository initialized successfully." << std::endl;
-  return true;
+    std::cout << "Repository initialized successfully." << std::endl;
+    return true;
 }
 
 bool handle_commit_command(const std::vector<std::string>& args) {
-  if (args.empty()) {
-    std::cerr << "Error: commit command requires a message argument." << std::endl;
-    return false;
-  }
+    if (args.empty()) {
+        std::cerr << "Error: commit command requires a message argument." << std::endl;
+        return false;
+    }
 
-  return vcs_commit(args[0]);
+    return vcs_commit(args[0]);
 }
 
 bool handle_diff_command(const std::vector<std::string>& args) {
-  if (!args.empty()) {
-    std::cerr << "Error: diff command does not take any arguments." << std::endl;
-    return false;
-  }
+    if (!args.empty()) {
+        std::cerr << "Error: diff command does not take any arguments." << std::endl;
+        return false;
+    }
 
-  std::cout << "Displaying differences between current state and latest commit" << std::endl;
+    std::cout << "Displaying differences between current state and latest commit" << std::endl;
 
-  return true;
+    return true;
 }
 
 bool handle_log_command(const std::vector<std::string>& args) {
+    if (!args.empty()) {
+        std::cerr << "Error: log command does not take any arguments." << std::endl;
+        return false;
+    }
+
+    vcs_log();
+
+    return true;
+}
+
+bool handle_help_command(const std::vector<std::string>& args) {
   if (!args.empty()) {
-    std::cerr << "Error: log command does not take any arguments." << std::endl;
+    std::cerr << "Error: help command does not take any arguments." << std::endl;
     return false;
   }
 
-  vcs_log();
+    vcs_help();
 
-  return true;
+    return true;
 }
